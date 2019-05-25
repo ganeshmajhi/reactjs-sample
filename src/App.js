b6668a5fc0e50ee0457fc5644dfd1b8d5762ff77
Ganesh Majhi <ganeshmajhi@gmail.com>
2019-05-26 00:29:45 +0530

    changed the app.js file

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="Logo" class="App-logo"/>
+        <p>Welcome to OpenSource Day</p>
+        <p>Date: 25/05/2019</p>
+        <p>Place: Intuit, Bellandur, Bangalore</p>
       </header>
     </div>
   );
